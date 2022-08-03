e35ed490cdb1c40cf02b7b20f76b6d23c282c240
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-02 21:46:03 -0300

    comandos basicos tipos de variveis

---
+++ comandosBasicosTiposDeVariaveis.kt
@@ -0,0 +1,8 @@
+// criação de uma função
+fun main() {
+    // constantes que armazenam o valor mínimo e máximo 
+    // que cada tipo de variável pode conter
+    println(Int.MIN_VALUE)
+    println(Int.MAX_VALUE)
+    println(Double.MAX_VALUE)
+}
